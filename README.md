d46bc2eb4bccf8d04e087e88339a7d9e11dcaabf
ugis70194 <ugis70194@yahoo.co.jp>
2023-10-01 20:40:23 +0900

    基本機能を実装

--- README.md
+++ README.md
@@ -1,1 +1,34 @@
 # smart-remocon-through-discord
+
+## 概要
+
+「リモコンどこいった？」をスマホをリモコン代わりにすることで解決する。  
+
+## インターフェース
+
+iosアプリのショートカットからHTTPリクエストを送信する。  
+送信するメッセージの内容を事前に登録しておくことでワンタップで諸々の操作が可能。
+
+## 詳細
+
+raspberryPi Zero 上には赤外線送受信回路を実装されている。  
+この回路を用いて、送信したい赤外線信号を事前に登録しておき、任意のタイミングで登録した赤外線信号を送信できるようにしておく。  
+
+また、raspberryPi上にdiscord botサーバーを立ち上げておく。  
+botを登録したチャンネルに対応するメッセージを送信することで、対応する赤外線信号を送信する仕組みが実装されている。  
+加えて、raspberryPi起動時にdiscord botサーバーを自動起動するためにserviceファイルを記述してある。  
+
+discordサーバーを用いる理由はwebhookを用いて、外部から簡単にraspberryPiにメッセージを送信できるためである。  
+
+
+## 備考
+
+WPA2-PSK/WPA3-SAEを使用している場合のWi-Fi設定は以下を参照。  
+
+https://raspida.com/wi-fi-setting-env#index_id6
+
+エアコンの信号が長すぎる場合の対応は以下を参照。  
+
+https://korintje.com/archives/28  
+
+http://www.neko.ne.jp/~freewing/raspberry_pi/raspberry_pi_gpio_pigpio_ir_remote_control/
---
+++ bot.py
@@ -0,0 +1,44 @@
+import discord
+import os
+import subprocess
+from time import sleep
+from dotenv import load_dotenv
+
+def send_signal(id: str):
+  subprocess.run(["python3", "./_irrp.py", "-p", "-g17", "-f", "./codes", id])
+
+load_dotenv(".env")
+
+TOKEN = os.getenv("TOKEN")
+print(TOKEN)
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_message(message):
+  if message.author == client.user:
+    return
+  
+  command = message.content.split("_")
+  if command[0] == "light":
+    if command[1] == "on":
+      send_signal("light:toggle")
+    elif command[1] == "off":
+      send_signal("light:toggle")
+      sleep(0.5)
+      send_signal("light:toggle")
+  elif command[0] == "aircon":
+    if command[1] == "cool":
+      send_signal("aircon:cool")
+    elif command[1] == "warm":
+      send_signal("aircon:warm")
+    elif command[1] == "off":
+      send_signal("aircon:off")
+    pass
+  else:
+    pass
+
+client.run(TOKEN)
