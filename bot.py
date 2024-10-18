bd9a19597d6f7073862b8bcde8a0d1dc8e33d780
ugis70194 <ugis70194@yahoo.co.jp>
2024-10-18 09:49:57 +0900

    Light用のデータ構造を追加 & それを使うように変更

--- bot.py
+++ bot.py
@@ -1,16 +1,7 @@
 import discord
 import os
-import subprocess
-import datetime
-from time import sleep
 from dotenv import load_dotenv
-
-def send_signal(id: str):
-  subprocess.run(["python3", "./_irrp.py", "-p", "-g17", "-f", "./codes", id])
-
-def reserve_signal(id: str, hour: str, minute: str):
-  command = f"python3 /home/pi/smart-remocon/_irrp.py -p -g17 -f ./codes {id}"
-  subprocess.run(f'echo {command} | at {hour}:{minute}')
+from Light import Light
 
 load_dotenv(".env")
 
@@ -22,33 +13,26 @@ intents.message_content = True
 
 client = discord.Client(intents=intents)
 
+light = Light()
+
 @client.event
 async def on_message(message):
   if message.author == client.user:
     return
   
   command = message.content.split("_")
-  if command[0] == "light":
-    if command[1] == "on":
-      send_signal("light:toggle")
-    elif command[1] == "off":
-      send_signal("light:toggle")
-      sleep(0.5)
-      send_signal("light:toggle")
+  if not (command[0] == "light" or command[0] == "aircon" or command[0] == "cleaner"):
+    return
+  
+  if   command[0] == "light":
+    if   command[1] == "on":       light.on()
+    elif command[1] == "off":      light.off()
+    elif command[1] == "night":    light.night()
+    elif command[1] == "toBright": light.toBright()
+    elif command[1] == "toDark":   light.toDark()
+    elif command[1] == "toWarm":   light.toWarm()
+    elif command[1] == "toCool":   light.toCool()
   elif command[0] == "aircon":
-    if command[1] == "cool":
-      if command[3] == "reserve":
-        reserve_signal("aircon:cool", command[4], command[5])
-      else:
-        send_signal("aircon:cool")
-    elif command[1] == "warm":
-      if command[3] == "reserve":
-        reserve_signal("aircon:warm", command[4], command[5])
-      else:
-        send_signal("aircon:warm")
-    elif command[1] == "off":
-      send_signal("aircon:off")
-  else:
     pass
 
 client.run(TOKEN)
